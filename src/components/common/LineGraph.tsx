eff7af3b1f0c4d439b82aa0a6770bc46b46f27b2
Tuur Lievens <tuur.lievens@hotmail.com>
2020-08-16 21:33:03 +0200

    Graph line first draft

--- src/components/common/LineGraph.tsx
+++ src/components/common/LineGraph.tsx
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 import { SeriesDataTypes } from "../../types/DataTypes";
 
-type GraphInputType = {
-    y: string; // label
-    series: { name: string; values: (number | null)[] }[];
-    xData: number[];
+type GraphSeriesType = {
+    id: string;
+    name: string;
+    color: string;
+    x: number[];
+    y: number[];
+};
+
+type GraphPointType = {
+    x: number;
+    y: number;
 };
 
 type PropsType = {
@@ -17,86 +25,100 @@ const LineGraph = (props: PropsType): JSX.Element => {
     const width = 600;
     const height = 300;
     const margin = { top: 20, right: 20, bottom: 30, left: 30 };
+    const tickSpread = 80;
 
     useEffect(() => {
         if (!graphElement.current) return;
-        // Format data
-        const data: GraphInputType = {
-            y: "",
-            series: props.data.series.map((item) => {
-                return { ...item, values: props.data.values.map((point) => point[item.id] || null) };
-            }),
-            xData: props.data.values.map((item) => item.x),
-        };
 
-        // Setup chart
+        const sortedValues = props.data.values.sort((a, b) => a.x - b.x);
+
+        // Format data
+        const data: GraphSeriesType[] = props.data.series.map((item) => {
+            const filteredValues = sortedValues.reduce<GraphPointType[]>((list, point, index) => {
+                if (point[item.id]) {
+                    list.push({
+                        x: point.x,
+                        y: point[item.id] as number,
+                    });
+                }
+                return list;
+            }, []);
+
+            return {
+                ...item,
+                x: filteredValues.map((point) => point.x),
+                y: filteredValues.map((point) => point.y),
+            };
+        });
+
+        // setup chart
         const svg = d3.select(graphElement.current);
+        svg.selectAll("*").remove();
 
         // x axis
-        const x = d3
+        const xMin = d3.min(sortedValues.map((point) => point.x)) || 0;
+        const xMax = d3.max(sortedValues.map((point) => point.x)) || 0;
+        const xScale = d3
             .scaleLinear()
-            .domain([0, 10])
+            .domain([xMin, xMax])
             .range([margin.left, width - margin.right]);
-        const xAxis = (g: any) =>
+
+        svg.append("g").call((g) =>
             g.attr("transform", `translate(0,${height - margin.bottom})`).call(
                 d3
-                    .axisBottom(x)
-                    .ticks(width / 80)
+                    .axisBottom(xScale)
+                    .ticks(width / tickSpread)
                     .tickSizeOuter(0),
-            );
-        svg.append("g").call(xAxis);
+            ),
+        );
 
         // y axis
-        const y = d3
+        const yMin = d3.min(data, (series) => d3.min(series.y)) || 0;
+        const yMax = d3.max(data, (series) => d3.max(series.y)) || 0;
+        const yScale = d3
             .scaleLinear()
-            .domain([0, d3.max(data.series, (d) => d3.max(d.values as any)) as any])
+            .domain([yMin, yMax])
             .nice()
             .range([height - margin.bottom, margin.top]);
 
-        const yAxis = (g: any) =>
+        svg.append("g").call((g) =>
             g
                 .attr("transform", `translate(${margin.left},0)`)
-                .call(d3.axisLeft(y))
-                .call((g: any) => g.select(".domain").remove())
-                .call((g: any) =>
+                .call(d3.axisLeft(yScale))
+                .call((g) => g.select(".domain").remove())
+                .call((g) =>
                     g
                         .select(".tick:last-of-type text")
                         .clone()
                         .attr("x", 3)
                         .attr("text-anchor", "start")
                         .attr("font-weight", "bold")
-                        .text(data.y),
-                );
-        svg.append("g").call(yAxis);
+                        .text(props.data.yLabel),
+                ),
+        );
 
-        // draw lines
-        const line = d3.line()
-            .defined((d: any) => !isNaN(d))
-            .x((d, i): any => x(data.xData[i]))
-            .y((d: any) => y(d));
+        // draw a single series line
+        const drawLine = (series: GraphSeriesType): string | null => {
+            const pointArray: [number, number][] = series.x.map((x, i) => [x, series.y[i]]);
+            return d3
+                .line()
+                .x((d) => xScale(d[0]))
+                .y((d) => yScale(d[1]))(pointArray);
+        };
 
-            // svg.append("path")
-            // .datum(data)
-            // .attr("fill", "none")
-            // .attr("stroke", "steelblue")
-            // .attr("stroke-width", 1.5)
-            // .attr("stroke-linejoin", "round")
-            // .attr("stroke-linecap", "round")
-            // .attr("d", line);
-
-        const path = svg
-            .append("g")
+        // draw all series lines
+        svg.append("g")
             .attr("fill", "none")
-            .attr("stroke", "red")
             .attr("stroke-width", 1.5)
             .attr("stroke-linejoin", "round")
             .attr("stroke-linecap", "round")
             .selectAll("path")
-            .data(data.series)
+            .data(data)
             .join("path")
-            .style("mix-blend-mode", "multiply")
-            .attr("d", (d) => line(d.values as any));
+            .attr("stroke", (series) => series.color)
+            .attr("d", (series) => drawLine(series));
 
+        // TODO: add hover effect
         // svg.call(hover, path);
     }, [props.data, margin]);
 
--- src/types/DataTypes.ts
+++ src/types/DataTypes.ts
@@ -3,6 +3,7 @@ type SeriesId = string;
 
 export type SeriesDataTypes = {
     xLabel: number;
+    yLabel: number;
     series: ({
         id: SeriesId;
         name: string;
